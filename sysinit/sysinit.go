ef24d9b6ca334cb9b39985138ae8502185ec9496
“lijiesheng” <ljs_hsm@163.com>
2022-07-13 15:58:48 +0800

    beego 读取非db.conf 文件

--- sysinit/sysinit.go
+++ sysinit/sysinit.go
@@ -1,23 +1,2 @@
 package sysinit
 
-import (
-	"fmt"
-	"github.com/astaxie/beego"
-)
-
-func registDatabase(alias string)  {
-	if len(alias) == 0 {
-		return
-	}
-	dbName := beego.AppConfig.String("db_" + alias + "_database")
-	dbUser := beego.AppConfig.String("db_" + alias + "_username")
-	dbPwd := beego.AppConfig.String("db_" + alias + "_password")
-	dbHost := beego.AppConfig.String("db_" + alias + "_host")
-	dbPort := beego.AppConfig.String("db_" + alias + "_port")
-
-	fmt.Println(dbName)
-	fmt.Println(dbUser)
-	fmt.Println(dbPwd)
-	fmt.Println(dbHost)
-	fmt.Println(dbPort)
-}
